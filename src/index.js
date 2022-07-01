243691405d4e9e8f8ee70add08047d41987a892e
nqv2291 <ngoquangviet2001@gmail.com>
2022-07-01 08:28:40 +0700

    add handlebars

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
 // HTTP logger
 app.use(morgan('combined'));
 
+// template engine
+app.engine('hbs', engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+// route
 app.get('/', (req, res) => {
-    res.send('hello world!')
+    res.render('home');
 });
 
 app.listen(port, () => {
