c3662ede919a34f23b330435cc600b25026f35de
nqv2291 <ngoquangviet2001@gmail.com>
2022-07-16 21:46:50 +0700

    add product image to database

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const {conn, sql} = require('../config/database');
 
+
 function route(app) {
     app.get('/', async (req, res) => {
       // http://localhost:3000/
@@ -7,7 +8,7 @@ function route(app) {
       var sqlString = "EXEC getProductGeneralInfo";
       return await pool.request().query(sqlString, function(err, data) {
         if (data.recordset != undefined) {
-          res.send(data);
+          res.send(data.recordset);
         }
       });
       
@@ -17,9 +18,34 @@ function route(app) {
       // eg: http://localhost:3000/COFT01P01
       var id =  req.params.id;
       var pool = await conn;
+      var sqlString = "SELECT image FROM Product WHERE ProductID = '" + id + "'";
+      return await pool.request().query(sqlString, function(err, data) {
+        if (data.recordset.at(0) != undefined) {
+          var rawData = data.recordset.at(0)["image"];
+          var data = rawData.replace(/^data:image\/png;base64,/, '');
+          var img = Buffer.from(data, 'base64');
+
+          res.writeHead(200, {
+            'Content-Type': 'image/png',
+            'Content-Length': img.length
+          });
+          res.end(img);
+
+        } else {
+          res.send("No image found");
+        }
+      });
+    });
+
+
+    app.get('/getImage', async (req, res) => {
+      // eg: http://localhost:3000/COFT01P01
+      var id =  req.params.id;
+      var pool = await conn;
       var sqlString = "EXEC getAllProductGeneralInfoByID '" + id + "'";
       return await pool.request().query(sqlString, function(err, data) {
         res.send(data.recordset);
+        
       });
     });
     
