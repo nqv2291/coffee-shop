dffda46a0d9a24fc839a95b80fddf05207246fe2
nqv2291 <ngoquangviet2001@gmail.com>
2022-07-21 18:35:02 +0700

    fix insert proc

---
+++ database/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Product (productID, categoryID, name, description, image, quantity, price)
+VALUES  ('COFT01P01', 'COFT01', 'Blue Boy', 
+            'Blue Boy is made of a blend of Guatemala, Ixlama and Colombia, Frontera de Planadas. The Guatemalan component is the culmination of work by over 100 family producers in the Guatemalan municipalities of San Pedro Necta and La Libertad in the department of Huehuetenango, a region known for rugged landscapes and high elevation. The Colombian component is sourced from several small quality farms in Colombias'' Tolima region. Together they form a classic blend.',
+            'data:image/png;base64,',
+            15, 17.00)
