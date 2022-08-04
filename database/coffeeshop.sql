bf6c6fc346cb98ef09a3220549bc12ec1d0c9584
nqv2291 <ngoquangviet2001@gmail.com>
2022-08-04 10:31:51 +0700

    add getAllSortedProducts

--- database/coffeeshop.sql
+++ database/coffeeshop.sql
@@ -150,6 +150,25 @@ SELECT productID, productName, image, quantity, price, rating, categoryID, categ
 FROM vwProductCategory
 GO
 
+CREATE PROC getAllSortedProducts
+@sortBy VARCHAR(30)
+AS
+BEGIN
+	DECLARE @order CHAR(4), @criteria VARCHAR(30), @sqlQuery VARCHAR(1000);
+	SET @order = SUBSTRING(@sortBy, 1, 3);
+	SET @criteria = SUBSTRING(@sortBy, 5, 30);
+	SET @sqlQuery = 'SELECT productID, productName, image, quantity, price, rating, categoryID, categoryName, parentID
+					FROM vwProductCategory
+					ORDER BY ' + @criteria;
+
+	IF @order = 'INC'
+		SET @sqlQuery = @sqlQuery + ' ASC';
+	ELSE IF @order = 'DEC'
+		SET @sqlQuery = @sqlQuery + ' DESC';
+	EXEC(@sqlQuery);
+END
+GO
+
 CREATE PROC getProductReviews
 @productID CHAR(9)
 AS
